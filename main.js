3f517627845e428d86885d25487b55a887950eb3
AaravSinha16 <83393367+AaravSinha16@users.noreply.github.com>
2021-04-29 13:02:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function one() {
+    window.alert("The Taj Mahal is well known across the world for its historical value, its tale of love, and its stunning beauty. The Taj Mahal is located in the historic Indian city of Agra. It houses the tomb of Mumtaz Mahal, the wife of the Mughal Emperor Shah Jahan. It is said that the Emperor loved his wife dearly and was prompted to build the Taj Mahal after her death as a testament to his love.");
+}
+
+function Two() {
+    window.alert("Rome, Italy, hosts the Colosseum, which is one of the world's seven wonders. Sometimes called the Flavian Amphitheatre, the Colosseum is an oval-shaped amphitheater in the center of the city. Constructed from concrete and sand, it is the largest amphitheater in the world. The Colosseum's construction was initiated in AD 72 by Emperor Vespasian and was finished by AD 80 by his successor, Titus. Domitian, another emperor of the Flavian dynasty later made certain modifications to the amphitheater. The labor of tens of thousands of slaves were used to build this magnificent structure. The Colosseum had the capacity to host about 80,000 spectators and 80 entrances at the time of its construction.");
+}
+
+function Three() {
+    window.alert("Chichen Itza is an archaeological site located in Mexico’s Yucatán State. It is a pre-Columbian city that was built during the Terminal Classic period by the Maya people. Chichen Itza's structures like the temples, arcades, and pyramids were sacred to the ancient Maya people. Chichen Itza is believed to have been one of the major cities of the ancient Mayan world and constructions in the city exhibit a variety of architectural styles.");
+}
+
+function Four() {
+    window.alert("The dream destination of millions of people across the world, Machu Picchu is one of the seven wonders of the world. It is located in the Cusco Region of Peru’s Machupicchu District. According to the majority of archaeologists, the Inca emperor Pachacuti built Machu Picchu as an estate around the year 1450. The site developed as a city but was abandoned a century later during the Spanish Conquest. The site remained largely unknown to the rest of the world until its discovery by the American explorer Hiram Bingham. A new theory suggests that Machu Pichu could have been the end stage of an ancient pilgrimage route of the Inca people.");
+}
+
+function Five() {
+    window.alert("The Art Deco-style Christ the Redeemer statue has been looming over the Brazilians from upon Corcovado  mountain in an awe-inspiring state of eternal blessing since 1931. The 130-foot reinforced concrete-and-soapstone was designed by Heitor de Silva Costa and cost approximatley $250,000 to build - much of the money was raised through donations. The statue has becamed an easily recognized icon for Rio and Brazil.");
+}
+
+function Six() {
+    window.alert("Petra, a Jordanian wonder, is also listed among the seven wonders of the world. Petra is also nicknamed as the “Rose City” due to the stone color from which it is carved. It has immense archaeological, historical, and architectural value that makes it a jaw-dropping tourist attraction. The water conduit system and the rock-cut architecture are the two most notable features of this ancient city. The water conduit system installed by the ancient Nabataeans helped give rise to a flourishing city in a desert. The Royal Tombs which are large mausoleums with impressive facades carved into rock are a major attraction in Petra.");
+}
+
+function Seven() {
+    window.alert("The Great Wall of China, a global tourist hotspot, is known across the world for its uniqueness, great length, and historical value. It is also considered to be one of the seven wonders of the world. The Great Wall of China is associated with thousands of years of Chinese history.A series of walls were initially built by Chinese empires and states over a period of many years, beginning as early as the 7th century BCE. Over 20 dynasties/states contributed to the building of the wall. These walls were then joined together to result in the Great Wall of China. It was not just a wall but a kind of fortification with watchtowers, beacon towers, trenches, etc., built at intervals as protection from enemy forces. The official length of the Great Wall of China is 21,196.18 km (13,170.7 mi). However, nearly one-third of the Great Wall has disappeared over time. UNESCO inscribed the site as a UNESCO World Heritage site in 1987.");
+}
